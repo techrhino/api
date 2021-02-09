7356a31a7da4e55156933897b77099d536d35256
PFC-Elyas <elyas@passivefirecompany.co.nz>
2021-02-10 10:36:05 +1300

    Removed Collection Types from corebp

--- server.js
+++ server.js
@@ -1,13 +1,2 @@
 const strapi = require('strapi');
 strapi().start();
-
-// const localtunnel = require('localtunnel');
-// (async () => {
-//     const tunnel = await localtunnel({ port: 1337 });
-
-//     console.log(tunnel.url)
-
-//     tunnel.on('close', () => {
-//         // tunnels are closed
-//     });
-// })();
